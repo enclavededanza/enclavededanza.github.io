18f24c4e16f7e16cdcc39c5a6721ff424bcc5a6c
WanManolo <oneman.rt@gmail.com>
2023-12-25 20:43:04 +0100

    release: update version

---
+++ assets/index-701a6639.js
@@ -0,0 +1,1 @@
+import{a as o}from"./index-23b52eab.js";import{Q as a}from"./index-d37f5a40.js";const s=()=>o("div",{className:"Home",children:o(a,{})});export{s as default};
---
+++ assets/index-b23dfe2c.js
@@ -0,0 +1,1 @@
+import{j as s,a as o}from"./index-23b52eab.js";import{Q as t}from"./index-d37f5a40.js";const e=()=>s("div",{className:"About",children:["About Us!",o(t,{})]});export{e as default};
