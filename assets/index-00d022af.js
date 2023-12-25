c61bd2f125facd126f98823d17dc30f143ed3735
WanManolo <oneman.rt@gmail.com>
2023-12-25 18:37:05 +0100

    release poc

---
+++ assets/index-00d022af.js
@@ -0,0 +1,1 @@
+import{j as s,a as o}from"./index-48e68a01.js";import{Q as t}from"./index-63db7dc4.js";const e=()=>s("div",{className:"About",children:["About Us!",o(t,{})]});export{e as default};
---
+++ assets/index-8d027f42.js
@@ -0,0 +1,1 @@
+import{a as o}from"./index-48e68a01.js";import{Q as a}from"./index-63db7dc4.js";const s=()=>o("div",{className:"Home",children:o(a,{})});export{s as default};
